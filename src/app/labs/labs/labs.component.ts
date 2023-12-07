c06cf5a5dc1a51e4ca34e36d2f0f946040469b68
gerardogarnica <gerardo.garnica@gmail.com>
2023-12-07 15:44:58 -0500

    Create the angular project.

---
+++ src/app/labs/labs/labs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [],
+  templateUrl: './labs.component.html'
+})
+export class LabsComponent {
+
+}
